7a3763c9261e88dbd413309c960713eb87835651
al-bimani <=>
2020-05-12 08:13:51 +0300

    fix: -type/+circular

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+const isc = require('is-circular');
+
 function _in(b, type, a, _of) {
     let result = null;
-    if (typeof b == type) {
-        if (b && typeof b == "object") {
-            result = b instanceof Array ? [] : {};
-            for (let key in b) {
-                let farq = _of(a[key]).in(b[key]);
-                if (farq != null) result[key] = farq;
-            }
-        } else {
-            result = a != b ? b : a == b ? null : a;
+    if (b && typeof b == "object") {
+        if (isc(a) || isc(b)) {
+            let err = new TypeError("circular structure not allowed");
+            throw err;
+        };
+        result = b instanceof Array ? [] : {};
+        for (let key in b) {
+            let farq = _of(a[key]).in(b[key]);
+            if (farq != null) result[key] = farq;
         }
-    } else return null;
+    } else {
+        result = a != b ? b : a == b ? null : a;
+    }
     return result;
 }
 
